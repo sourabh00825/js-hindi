0caa57a97c176175de631f87c824379cab8225b0
sourabh00825 <skjamia123@gmail.com>
2024-12-28 08:55:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version 
+
+ // alert(3+3) // we are using nodejs, not browser
+
+ console.log(3 + 3 )
+ console.log("sourabh")  
+
+ let name = "sourabh"
+ let age = 21 
+ let isloggedIn = false
+
+
+ // number => 2 to power 53 
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standlone value
+ // undefined =>
+ // symbol => unique
+  
+ 
+
+ // object
+
+ console.log(typeof "sourabh");
+ console.log(typeof null); // object 
+ console.log(typeof undefined); // undefined
