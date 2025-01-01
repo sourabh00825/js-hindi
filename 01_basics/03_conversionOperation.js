1ba28d9317d89b4f381a88f09ba20b236d5b8a0f
sourabh00825 <skjamia123@gmail.com>
2025-01-01 08:15:09 +0000

    conversionof datatpe

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" =. Nan
+// true =. 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
