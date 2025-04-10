789bd104ac3c73baf3f650042d91d5c3c00a4d6d
sourabh00825 <skjamia123@gmail.com>
2025-04-10 08:29:33 +0000

    01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 223344
+let accountEmail = "sourabhk222@gmail.com"
+var accountPassword = "22332222"
+accountCity = "Delhi"
+
+accountEmail = "chikoo22@gmail.com"
+accountPassword = "11223344"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountEmail, accountEmail, accountPassword, accountCity])
+
