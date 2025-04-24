34f6d83145a08db07c60a62f83d820f1ba769b88
sourabh00825 <skjamia123@gmail.com>
2025-04-24 07:36:15 +0000

    01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,11 +2,19 @@ const accountId = 223344
 let accountEmail = "sourabhk222@gmail.com"
 var accountPassword = "22332222"
 accountCity = "Delhi"
+accountState = "kalkaji"
+accountColour = "red"
+accountNo = "0619100100001325"
+accountHolderName = "sourabh"
 
 accountEmail = "chikoo22@gmail.com"
 accountPassword = "11223344"
 accountCity = "Pune"
+accountState = "sangam vihar"
+accountColour = "black"
+accountNo = "0619100100001425"
+accountHolderName = "chikoo"
 
 console.log(accountId);
-console.table([accountEmail, accountEmail, accountPassword, accountCity])
+console.table([accountEmail, accountState, accountPassword, accountCity, accountColour, accountNo, accountHolderName])
 
